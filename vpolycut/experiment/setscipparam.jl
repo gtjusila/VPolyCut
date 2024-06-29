01ee804919e7e16170b386563d06c0b43dbeabe7
Gennesaret Tjusila <gennesaretktjusila@gmail.com>
2024-06-29 02:42:26 +0200

    Revert "Added parameter --easy"
    This reverts commit a0fd4f86e5d087f8e5a8b8a7cef39484f998bfdf.

--- vpolycut/experiment/setscipparam.jl
+++ vpolycut/experiment/setscipparam.jl
@@ -4,7 +4,7 @@
 #
 import SCIP
 
-function setscipsettings(setter::Function)
+function setscipsettings(setter::Function, easy::Bool)
     # Turn off heuristics
     setter("heuristics/padm/freq", -1)
     setter("heuristics/ofins/freq", -1)
@@ -105,8 +105,10 @@ function setscipsettings(setter::Function)
     setter("branching/relpscost/initcand", 0)
 
     # Uncomment the following for super easy instances
-    setter("propagating/maxroundsroot", 0)
-    setter("presolving/maxrounds", 0)
+    if easy
+        setter("propagating/maxroundsroot", 0)
+        setter("presolving/maxrounds", 0)
+    end
 end
 
 function includegomorysepa(setter::Function)
