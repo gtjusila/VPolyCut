0dea71d36f39dbe5e9a5e50f52c60175012653c7
Gennesaret Tjusila <gennesaretktjusila@gmail.com>
2024-07-06 18:41:00 +0200

    Uncommented feasibility test

--- vpolycut/experiment/execute.jl
+++ vpolycut/experiment/execute.jl
@@ -44,14 +44,11 @@ function execute(settings::Dict)
 
 
     # STEP 5: Check if debug solution is still feasible:
-    #=
     debug_sol = Ref{Ptr{SCIP.SCIP_Sol}}(C_NULL)
     feasible = Ref{SCIP.SCIP_Bool}(C_NULL)
     load_solution(scip, debug_sol, path * ".sol")
     SCIP.@SCIP_CALL SCIP.SCIPcheckSol(scip, debug_sol[], true, true, true, true, true, feasible)
     feasible = feasible[]
-    =#
-    feasible = SCIP.SCIP_Bool(true)
 
     writeoutput(result_path, scip, settings, reference_obj, feasible)
 end
