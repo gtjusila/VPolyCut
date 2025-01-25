2590f28438e2b15b2123f52fd97f06fe4feca5b5
Gennesaret Tjusila <gennesaretktjusila@gmail.com>
2025-01-25 19:00:22 +0100

    Gomory experiment Script

---
+++ scripts/gomory.jl
@@ -0,0 +1,77 @@
+using ArgParse
+using SCIP
+using JuMP
+using JSON
+using VPolyhedralCut.SCIPJLUtils
+
+function main()
+    ### Command Line Argument ###
+    args_setting = ArgParseSettings(;
+        description="Solve the LP relaxation of the instance, do 1 round of gomory cuts and get the gap closed information. Output will be a results.json file containing gap information and scip solving statistic"
+    )
+    @add_arg_table args_setting begin
+        "--instance", "-i"
+        help = "An instance to be solve"
+        required = true
+        "--output_dir", "-o"
+        help = "A directory where to writ results.json and scip_solving_statistic.txt"
+    end
+    parameter = ArgParse.parse_args(args_setting)
+    println(parameter)
+
+    # Use VPolyhedralCut.SCIPJLUtils to create SCIP Optimizer object
+    model = setup_scip_safe_jump_model()
+    scip = get_scip_data_from_model(model)
+
+    # Setup SCIP object parameter
+    set_heuristics_emphasis_off(model)
+    set_separators_emphasis_off(model)
+    set_cut_selection_off(model)
+    set_strong_branching_lookahead_off(model)
+
+    JuMP.set_attribute(model, "limits/restarts", 0)
+    JuMP.set_attribute(model, "limits/nodes", 1)
+    JuMP.set_attribute(model, "limits/time", 3600) # Time Limit is usually not an issue for gomory runs
+    JuMP.set_attribute(model, "separating/maxroundsroot", 1)
+    JuMP.set_attribute(model, "display/verblevel", 0)
+
+    # Turn on gomory cut
+    JuMP.set_attribute(model, "separating/gmi/freq", 0)
+    JuMP.set_attribute(model, "separating/gmi/priority", 9999)
+    JuMP.set_attribute(model, "separating/gmi/maxsuppabs", 5000)
+    JuMP.set_attribute(model, "separating/gmi/dynamiccuts", false)
+    JuMP.set_attribute(model, "separating/gmi/maxsupprel", 1.0)
+    JuMP.set_attribute(model, "separating/gmi/forcecuts", true)
+
+    # Read Problem
+    instance_path = abspath(parameter["instance"])
+    @info "Loading instance from $instance_path"
+    SCIP.@SCIP_CALL SCIP.SCIPreadProb(scip, instance_path, C_NULL)
+
+    # Solve
+    @info "Starting solve"
+    SCIP.@SCIP_CALL SCIP.SCIPsolve(scip)
+
+    # Write Output
+    @info "Writing Result"
+    result = Dict{String,Any}()
+    result["separator"] = "gomory"
+    result["instance"] = instance_path
+    result["scip_status"] = SCIP.SCIPgetStatus(scip)
+    result["initial_lp_obj"] = SCIP.SCIPgetFirstLPDualboundRoot(scip)
+    result["final_lp_obj"] = SCIP.SCIPgetDualboundRoot(scip)
+
+    output_path = abspath(parameter["output_dir"])
+    result_path = joinpath(output_path, "results.json")
+    open(result_path, "w") do io
+        JSON.print(io, result, 4)
+    end
+
+    solving_statistic_path = joinpath(output_path, "scip_solving_statistic.txt")
+    open(solving_statistic_path, "w") do io
+        solv_stats_file_ptr = Libc.FILE(io)
+        SCIP.SCIPprintStatistics(scip, solv_stats_file_ptr)
+    end
+end
+
+main()
---
+++ scripts/setup_experiment.jl
@@ -0,0 +1,93 @@
+using DataFrames
+using CSV
+using Mustache
+
+println("Setup experiment")
+
+instance_list = ""
+while true
+    print("Instance List Path: ")
+    global instance_list = abspath(readline())
+    if (isfile(instance_list))
+        break
+    else
+        println("Invalid Path.")
+    end
+end
+
+instance_dir = ""
+while true
+    print("Instance Directory: ")
+    global instance_dir = abspath(readline())
+    if (isdir(instance_dir))
+        break
+    else
+        println("Invalid Path.")
+    end
+end
+
+mode = ""
+while true
+    print("Mode: ")
+    global mode = readline()
+    if mode == "gomory"
+        break
+    else
+        println("Invalid Mode.")
+    end
+end
+
+runs_path = abspath(joinpath(dirname(@__FILE__), "..", "experiment_runs"))
+if !isdir(runs_path)
+    @info "Creating experiment_runs folder"
+    mkdir(runs_path)
+end
+
+experiment_path = ""
+while true
+    print("Experiment Name: ")
+    experiment_name = readline()
+    global experiment_path = abspath(joinpath(runs_path, experiment_name))
+    if !isdir(experiment_path)
+        mkdir(experiment_path)
+        break
+    end
+end
+
+instances = readlines(instance_list)
+
+if mode == "gomory"
+    ### Creating Config TSV ###
+    instances_path = [joinpath(instance_dir, instance * ".mps") for instance in instances]
+    output_path = [joinpath(experiment_path, instance) for instance in instances]
+    for path in output_path
+        mkdir(path)
+    end
+    config_dataframe = DataFrame(;
+        id=1:length(instances),
+        instances_path=instances_path,
+        output_path=output_path
+    )
+    run_settings_file = joinpath(experiment_path, "experiment_list.tsv")
+    CSV.write(run_settings_file, config_dataframe; delim='\t')
+
+    for path in instances_path
+        if (!isfile(path))
+            @warn "Possibly missing instance $(path)"
+        end
+    end
+
+    # Read the bash template file and write to the experiment folder
+    template_file = joinpath(@__DIR__, "gomory_template.sh")
+    data = Dict(
+        "N" => length(instances),
+        "EXPERIMENT_PATH" => experiment_path
+    )
+    bash_template = read(template_file, String)
+    bash_script = Mustache.render(bash_template, data)
+    output_file = joinpath(experiment_path, "job_script.sh")
+    open(output_file, "w") do f
+        write(f, bash_script)
+    end
+    run(`sbatch $output_file`)
+end
