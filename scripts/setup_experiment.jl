e463bd70834b05c777f7359555b74d1cbb8038f0
Gennesaret Tjusila <gennesaretktjusila@gmail.com>
2025-01-27 19:38:26 +0100

    Clean Up Script

--- scripts/setup_experiment.jl
+++ scripts/setup_experiment.jl
@@ -1,59 +1,49 @@
 using DataFrames
 using CSV
 using Mustache
+include("utils.jl")
 
 println("Setup experiment")
 
-instance_list = ""
-while true
-    print("Instance List Path: ")
-    global instance_list = abspath(readline())
-    if (isfile(instance_list))
-        break
-    else
-        println("Invalid Path.")
-    end
-end
+instance_list = prompt_user(;
+    message="Instance List",
+    validation=(x) -> isfile(abspath(x)),
+    error_message="Invalid Path.",
+    default="experiment_data/instances/instances_list.txt"
+)
 
-instance_dir = ""
-while true
-    print("Instance Directory: ")
-    global instance_dir = abspath(readline())
-    if (isdir(instance_dir))
-        break
-    else
-        println("Invalid Path.")
-    end
-end
+instance_dir = prompt_user(;
+    message="Instance List",
+    validation=(x) -> isdir(abspath(x)),
+    error_message="Invalid Path.",
+    default="experiment_data/instances"
+)
 
-mode = ""
-while true
-    print("Mode: ")
-    global mode = readline()
-    if mode == "gomory" || mode == "vpc"
-        break
-    else
-        println("Invalid Mode.")
-    end
-end
+mode = prompt_user(;
+    message="Mode",
+    validation=(x) -> x in ["vpc", "gomory"],
+    error_message="Invalid Mode.",
+    default="vpc"
+)
 
+# If experiment_runs is not there create it first.
 runs_path = abspath(joinpath(dirname(@__FILE__), "..", "experiment_runs"))
 if !isdir(runs_path)
     @info "Creating experiment_runs folder"
     mkdir(runs_path)
 end
 
-experiment_path = ""
-while true
-    print("Experiment Name: ")
-    experiment_name = readline()
-    global experiment_path = abspath(joinpath(runs_path, experiment_name))
-    if !isdir(experiment_path)
-        mkdir(experiment_path)
-        break
-    end
-end
+experiment_path = prompt_user(;
+    message="Experiment Name:",
+    validation=(x) ->
+        ((x != "") && !isdir(joinpath(runs_path, x))),
+    error_message="Invalid Mode.",
+    default=""
+)
+experiment_path = joinpath(runs_path, experiment_path)
+mkdir(experiment_path)
 
+### Done with basic parameter, start reading input ###
 instances = readlines(instance_list)
 
 if mode == "gomory"
@@ -113,7 +103,7 @@ elseif mode == "vpc"
     end
 
     # Read the bash template file and write to the experiment folder
-    template_file = joinpath(@__DIR__, "vpc_template.sh")
+    template_file = joinpath(@__DIR__, "job_templates", "vpc_template.sh")
     data = Dict(
         "N" => length(instances),
         "EXPERIMENT_PATH" => experiment_path
