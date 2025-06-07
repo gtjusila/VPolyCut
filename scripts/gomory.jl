62f17ebc44f63f3d00f7885ecd04c400a5c61463
Gennesaret Tjusila <gennesaretktjusila@gmail.com>
2025-06-07 20:52:25 +0200

    Removed support limit

--- scripts/gomory.jl
+++ scripts/gomory.jl
@@ -74,6 +74,9 @@ function main()
     JuMP.set_attribute(model, "separating/gmi/priority", 9999)
     JuMP.set_attribute(model, "separating/gmi/forcecuts", true)
     JuMP.set_attribute(model, "separating/gmi/separaterows", false)
+    JuMP.set_attribute(model, "separating/gmi/maxsuppabs", 99999999)
+    JuMP.set_attribute(model, "separating/gmi/dynamiccuts", false)
+    JuMP.set_attribute(model, "separating/gmi/maxsupprel", 1.0)
     JuMP.set_attribute(model, "separating/gmi/maxsepacutsroot", 2000)
 
     # Read Problem
